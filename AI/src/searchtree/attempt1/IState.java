212de08ffe214dc7f87595193dc887b452943a4e
NiklasWahrenberg <niklas.wahrenberg@gmx.de>
2018-04-13 18:08:03 +0200

    Deleted some old files

--- AI/src/searchtree/attempt1/IState.java
+++
@@ -1,8 +0,0 @@
-package searchtree.attempt1;
-
-public interface IState<T>
-{
-	public abstract T getValue();
-	public abstract boolean equals(IState<T> toBeCompared);
-	public abstract int hashCode();
-}
